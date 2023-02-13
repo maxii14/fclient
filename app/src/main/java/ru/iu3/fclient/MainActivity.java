790e4e857bf4657dd7bb4b95bac5f25d49602491
Max <max.lei@bk.ru>
2023-02-13 22:26:19 +0300

    proj

---
+++ app/src/main/java/ru/iu3/fclient/MainActivity.java
@@ -0,0 +1,49 @@
+package ru.iu3.fclient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import ru.iu3.fclient.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Used to load the 'fclient' library on application startup.
+    static {
+        System.loadLibrary("fclient");
+        System.loadLibrary("mbedcrypto");
+    }
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        int res = initRng();
+        byte[] v = randomBytes(10);
+
+        //Работает!!
+        for(int i = 0; i < 10; i++) {
+            Log.d("bytes", v[i] + ", ");
+        }
+
+        // Example of a call to a native method
+        TextView tv = binding.sampleText;
+        tv.setText(stringFromJNI());
+    }
+
+    /**
+     * A native method that is implemented by the 'fclient' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+    public static native int initRng();
+    public static native byte[] randomBytes(int no);
+    public static native byte[] encrypt(byte[] key, byte[] data);
+    public static native byte[] decrypt(byte[] key, byte[] data);
+}
