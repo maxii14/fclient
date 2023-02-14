be4a5a3c30fc8bbc8fde93a15424685395f2558b
Max <max.lei@bk.ru>
2023-02-14 20:33:59 +0300

    Добавили тест функций encrypt и decrypt

--- app/src/main/java/ru/iu3/fclient/MainActivity.java
+++ app/src/main/java/ru/iu3/fclient/MainActivity.java
@@ -6,6 +6,9 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
 
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+
 import ru.iu3.fclient.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
@@ -25,17 +28,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         int res = initRng();
-        byte[] v = randomBytes(10);
+        byte[] v = randomBytes(16);
 
         //Работает!!
-        for(int i = 0; i < 10; i++) {
+        for(int i = 0; i < v.length; i++) {
             Log.d("bytes", v[i] + ", ");
         }
+
+        //Передаём 16-байтовый ключ, преобразуем строку в байты и передаём в функцию encrypt
+        String codedStr = "code this";
+        byte [] codedStrBytes = codedStr.getBytes(StandardCharsets.UTF_8);
+        byte [] coded = encrypt(v, codedStrBytes);
+
+        //Передаём в функцию decrypt тот же ключ и закодированные байты
+        String decoded = new String(decrypt(v, coded), StandardCharsets.UTF_8);
+        Log.d("decoded", decoded); //Получаем исходную строку
 
         // Example of a call to a native method
         TextView tv = binding.sampleText;
         tv.setText(stringFromJNI());
     }
+
 
     /**
      * A native method that is implemented by the 'fclient' native library,
