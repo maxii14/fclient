4b6497ae0388910383790d9e4bb0a117bced93e5
Max <max.lei@bk.ru>
2023-04-16 15:08:35 +0300

    7 laba

--- src/main/java/ru/iu3/backend/repositories/UserRepository.java
+++ src/main/java/ru/iu3/backend/repositories/UserRepository.java
@@ -4,7 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.iu3.backend.models.User;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    Optional<User> findByLogin(String login);
+
+    Optional<User> findByToken(String valueOf);
 }
